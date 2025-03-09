f6b4b1d2442425a6d898c13784d6a5c5196e88fb
klaus-ubuntu <klausjurgenfolz@gmail.com>
2025-03-09 20:19:02 +0100

    Included buy-and-hold return calculation

--- ObelixTrade/backtester.py
+++ ObelixTrade/backtester.py
@@ -45,3 +45,25 @@ class Backtester:
         final_equity = balance + btc_held * df_with_signals['close'].iloc[-1]
         df_with_signals['equity_curve'] = equity_curve
         return final_equity
+    
+    def buy_and_hold_return(self, df: pd.DataFrame) -> float:
+        """
+        Calculates the final equity if you bought at the very beginning and sold at the very last point.
+        
+        - Buys at the first available close price (after applying the fee).
+        - Sells at the last available close price (after applying the fee).
+        """
+        if df.empty:
+            return self.initial_balance
+        
+        # Get the first and last close prices
+        first_price = df['close'].iloc[0]
+        last_price = df['close'].iloc[-1]
+        
+        # Simulate buy: subtract fee and buy BTC at the first price
+        balance_after_buy_fee = self.initial_balance * (1 - self.fee_rate)
+        btc_bought = balance_after_buy_fee / first_price
+        
+        # Simulate sell: sell BTC at the last price after subtracting fee
+        final_balance = (btc_bought * last_price) * (1 - self.fee_rate)
+        return final_balance
